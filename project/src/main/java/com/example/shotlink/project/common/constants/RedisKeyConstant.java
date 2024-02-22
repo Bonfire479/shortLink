507908ce7c6035fd079deef9aae5f19313b8e5ae
bonfire479 <2936794055@qq.com>
2024-02-22 19:52:44 +0800

    refactor: 重构RedisKey加锁结构

--- project/src/main/java/com/example/shotlink/project/common/constants/RedisKeyConstant.java
+++ project/src/main/java/com/example/shotlink/project/common/constants/RedisKeyConstant.java
@@ -1,8 +1,8 @@
 package com.example.shotlink.project.common.constants;
 
 public class RedisKeyConstant {
-    public static final String LOCK_USER_REGISTER = "shortLink:lock:user_register:";
+    public static final String LOCK_USER_REGISTER = "lock:shortLink:user_register:";
     public static final String USER_LOGIN_TOKEN = "shortLink:user_login_token:";
     public static final String SHORT_LINK_KEY = "shortLink:";
-    public static final String LOCK_SHORT_LINK_KEY = "shortLink:lock:";
+    public static final String LOCK_SHORT_LINK_KEY = "lock:shortLink:";
 }
