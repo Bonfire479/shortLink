d4475d3f1a3711cf13dea657f510912a69466193
bonfire479 <2936794055@qq.com>
2024-02-22 17:38:05 +0800

    项目规约以及自定义异常结构

---
+++ project/src/main/java/com/example/shotlink/project/common/convention/exception/RemoteException.java
@@ -0,0 +1,31 @@
+package com.example.shotlink.project.common.convention.exception;
+
+
+import com.example.shotlink.project.common.convention.error.IErrorCode;
+import com.example.shotlink.project.common.enums.BaseErrorCode;
+
+/**
+ * 远程服务调用异常
+ */
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
