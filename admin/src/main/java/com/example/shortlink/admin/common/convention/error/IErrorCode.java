caa245b2e68d10895adddab9e454ae33fcdc1099
bonfire479 <2936794055@qq.com>
2024-02-22 17:40:23 +0800

    feature: 异常码抽象

---
+++ admin/src/main/java/com/example/shortlink/admin/common/convention/error/IErrorCode.java
@@ -0,0 +1,9 @@
+package com.example.shortlink.admin.common.convention.error;
+
+
+public interface IErrorCode {
+    public String code();
+    public String message();
+
+}
+
---
+++ project/src/main/java/com/example/shotlink/project/common/convention/error/IErrorCode.java
@@ -0,0 +1,9 @@
+package com.example.shotlink.project.common.convention.error;
+
+
+public interface IErrorCode {
+    public String code();
+    public String message();
+
+}
+
